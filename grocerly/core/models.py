62f0715c5b51280d9941e5234debbd9784ad23eb
Le Duc Phat <phat.lduc@gmail.com>
2025-09-05 15:54:59 +0700

    added Vendor and Category classes

--- grocerly/core/models.py
+++ grocerly/core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+from userauths.models import User
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 # Create your models here.
+class Category(models.Model):
+    c_id = ShortUUIDField(unique=True, length=10, max_length=20, prefix="cat", alphabet="abcdefgh12345")
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="category")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def category_image(self):
+        return mark_safe(f'<img src="{self.image.url}" width="50" height="50" />')
+    
+    def __str__(self):
+        return self.title
+    
+
+class Vendor(models.Model):
+    v_id = ShortUUIDField(unique=True, length=10, max_length=20, prefix="ven", alphabet="abcdefgh12345")
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=user_directory_path)
+    description = models.TextField(null=True, blank=True)
+
+    address = models.CharField(max_length=100, default="123 Main Street")
+    contact = models.CharField(max_length=100, default="+123 (456) 789")
+    chat_resp_time = models.CharField(max_length=100, default="100")
+    shipping_on_time = models.CharField(max_length=100, default="100")
+    authentic_rating = models.CharField(max_length=100, default="100")
+    days_return = models.CharField(max_length=100, default="100")
+    warranty_period = models.CharField(max_length=100, default="100")
+
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def vendor_image(self):
+        return mark_safe(f'<img src="{self.image.url}" width="50" height="50" />')
+    
+    def __str__(self):
+        return self.name
