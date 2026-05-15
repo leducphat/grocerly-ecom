a0720dd7cb2c5614f2195474136b9f8ef6b4f259
Le Duc Phat <phat.lduc@gmail.com>
2026-05-15 17:20:45 +0700

    Use STORAGES dict for file backends
    Replace legacy DEFAULT_FILE_STORAGE/STATICFILES_STORAGE settings with the Django STORAGES mapping. When USE_CLOUDINARY is enabled, set the default backend to Cloudinary and staticfiles to Whitenoise; otherwise use the local FileSystemStorage as default and keep Whitenoise for static files. Preserve MEDIA_URL/MEDIA_ROOT for local media; remove the old STATICFILES_STORAGE assignment.

--- grocerly/grocerly/settings.py
+++ grocerly/grocerly/settings.py
@@ -195,13 +195,17 @@ if USE_CLOUDINARY:
     import cloudinary.uploader
     import cloudinary.api
     
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES = {
+        "default": {"BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage"},
+        "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    }
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# Whitenoise for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+        "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+        "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
